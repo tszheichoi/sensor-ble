93d2993551ac61908c5f2043b7478b1d05704d71
Michael Haberler <mail17@mah.priv.at>
2025-04-20 17:20:27 +0200

    Merge pull request #1 from mhaberler/bthome
    proposal: initial support for servicedata - type sensors like BTHome devices, matching by serviceUUID

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { decoder as airpods } from "./devices/airpods.js";
 import { decoder as ruuvi } from "./devices/ruuvi.js";
 import { decoder as muse_v3 } from "./devices/muse_v3.js";
+import { decoder as bthome } from "./devices/bthome.js";
 
-export const decoders = [ruuvi, muse_v3, airpods];
+export const decoders = [ruuvi, muse_v3, airpods, bthome];

18365df01679375ba938b6645e559c525fbab5a1
Kelvin Choi <tszheichoi@gmail.com>
2026-06-26 14:51:10 +0100

    initial cscs attempt

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ import { decoder as ruuvi } from "./devices/ruuvi.js";
 import { decoder as muse_v3 } from "./devices/muse_v3.js";
 import { decoder as bthome } from "./devices/bthome.js";
 import { decoder as mopeka } from "./devices/mopeka.js";
+import { decoder as cscs } from "./devices/cscs.js";
 
-export const decoders = [ruuvi, muse_v3, airpods, bthome, mopeka];
+export const decoders = [ruuvi, muse_v3, airpods, bthome, mopeka, cscs];
